ec43e7ebd54267f425118df46addec8cd6d82cf8
jeongwoo1020 <ndn1020@gmail.com>
2024-10-16 18:46:56 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+
+
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = two_sum(nums, target)
+    print(result)
